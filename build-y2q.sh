8928c1d9d504e1f235d5b9fbda618de9382af10d
dreamboy001226 <dreamboy001226@gmail.com>
2026-04-10 18:34:47 +0500

    modified .sh

--- build-y2q.sh
+++ build-y2q.sh
@@ -14,12 +14,13 @@ KERNEL_MAKE_ENV="DTC_EXT=$(pwd)/tools/dtc CONFIG_BUILD_ARM64_DT_OVERLAY=y"
 # Install clang toolchain
 mkdir -p clang
 cd clang || exit 1
-wget -q https://github.com/ZyCromerZ/Clang/releases/download/21.0.0git-20250228-release/Clang-21.0.0git-20250228.tar.gz
-tar -xf Clang*
+wget -q https://github.com/ZyCromerZ/Clang/releases/download/21.0.0git-20250228-release/Clang-21.0.0git-20250228.tar.gz -O clang.tar.gz
+tar -xf clang.tar.gz
 cd "$KERNEL_DIR" || exit 1
 PATH="${KERNEL_DIR}/clang/bin:$PATH"
 
 # Cross compile options
+MAKE_OPT=()
 MAKE_OPT+=(CROSS_COMPILE=aarch64-linux-gnu- CROSS_COMPILE_ARM32=arm-linux-gnueabi-)
 
 # Clean previous outputs
@@ -39,7 +40,9 @@ echo "*****************************************"
 # Build DTBs
 make -j"$(nproc)" O="$OUT_DIR" ARCH=arm64 LLVM=1 LLVM_IAS=1 "${MAKE_OPT[@]}" dtbs
 DTB_OUT="$OUT_DIR/arch/arm64/boot/dts/vendor/qcom"
-cat $DTB_OUT/*.dtb > "$ANYKERNEL_DIR/dtb"
+if ls "$DTB_OUT"/*.dtb 1> /dev/null 2>&1; then
+    cat "$DTB_OUT"/*.dtb > "$ANYKERNEL_DIR/dtb"
+fi
 
 # Build Kernel Image
 make -j"$(nproc)" O="$OUT_DIR" ARCH=arm64 LLVM=1 LLVM_IAS=1 "${MAKE_OPT[@]}" Image
@@ -53,7 +56,7 @@ cp "$IMAGE" "$ANYKERNEL_DIR/Image"
 
 # Package into zip
 cd "$ANYKERNEL_DIR"
-rm -f *.zip
-zip -r9 "${KERNEL_NAME}$(date +"%Y%m%d")+y2q.zip" .
+rm -f *.zip *.img Image dtb
+zip -r9 "${KERNEL_NAME}$(date +"%Y%m%d")+y2q.zip" . -x "*.git*" -x "README.md"
 
 echo "The bomb has been planted."
